1011ba498f938042c1bcec4873f2fd052e6dbe90
liyanyan <liyanyan@tftnet.cn>
2019-07-11 15:35:26 +0800

    二次

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -24,7 +24,7 @@
       <el-table-column prop="create_time" label="创建时间" width="180"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button class="operatingbtn" size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button class="operatingbtn" size="mini" @click="handleEdit(scope.$index, scope.row)">编辑 </el-button>
           <el-button class="operatingbtn"  size="mini" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
         </template>
       </el-table-column>
